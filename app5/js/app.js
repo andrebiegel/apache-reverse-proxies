f214138394cc6e13c7e2842a5420c70194ce7842
André Biegel <andre.biegel@icloud.com>
2019-09-27 21:47:07 +0200

    initial commit

---
+++ app5/js/app.js
@@ -0,0 +1,5 @@
+import Config from './Config.js';
+
+//navigator.serviceWorker.register('/stockz-worker.js').
+//    then(registration => console.log('registration succeeded', registration)).
+//    catch(error => console.error('registration not successful',error));
